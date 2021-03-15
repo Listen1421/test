ee674edeeab40992a1137eebc34df925ebbb3ce4
李李是先生 <1421659925@qq.com>
2021-03-15 09:16:44 +0800

    增加公共类

---
+++ Common/ListCheck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class ListCheck
+    {
+        public static bool CheckList<T>(IList<T> ts)
+        {
+            if (ts.Count > 0 && ts != null)
+                return true;
+            else
+                return false;
+        }
+    }
+}
